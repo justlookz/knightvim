4890436248256e879096ae5ae51b1a0d4cdd2d41
justlookz <cspios@gmail.com>
2023-07-24 23:42:14 +0300

    user auto creation

--- lua/knightvim/init.lua
+++ lua/knightvim/init.lua
@@ -6,6 +6,17 @@ kvim.builtins = {}
 kvim.lsp = {}
 kvim.lsp.local_include = {}
 
+if vim.fn.isdirectory("user") == 0 then
+    vim.fn.mkdir("user")
+end
+
+if vim.fn.exists("user.init.lua") == 0 then
+    local file = io.open("user/init.lua", "w")
+    file:write(
+        "-- this is where you create your own configuration./n-- This is something that I am not gonna change here")
+    file:close()
+end
+
 require "user"
 
 require("knightvim.options")
