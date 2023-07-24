de7fbfc4f3ee5d8fa074ec5eed062bc8eb846f7f
justlookz <cspios@gmail.com>
2023-07-25 02:02:28 +0300

    fixed huge bug with autogenerate user and init for user

--- lua/knightvim/init.lua
+++ lua/knightvim/init.lua
@@ -6,12 +6,12 @@ kvim.builtins = {}
 kvim.lsp = {}
 kvim.lsp.local_include = {}
 
-if vim.fn.isdirectory("user") == 0 then
-    vim.fn.mkdir("user")
+if (vim.fn.isdirectory( vim.fn.stdpath("config") .. "/lua/user")) == 0 then
+    vim.fn.mkdir( vim.fn.stdpath("config") ..  "/lua/user")
 end
 
-if vim.fn.exists("user.init.lua") == 0 then
-    local file = io.open("user/init.lua", "w")
+if (vim.fn.exists( vim.fn.stdpath("config") .. "/lua/user/init.lua")) == 0 then
+    local file = io.open( vim.fn.stdpath("config") .. "/lua/user/init.lua", "w")
     file:write(
         "-- this is where you create your own configuration./n-- This is something that I am not gonna change here")
     file:close()
