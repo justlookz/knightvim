65c61b3ecca39a441f46d7314e1283e0afbe92e7
justlookz <cspios@gmail.com>
2023-07-25 16:18:12 +0300

    cleanup code and fixed some readability

--- lua/knightvim/init.lua
+++ lua/knightvim/init.lua
@@ -7,12 +7,15 @@ kvim.lsp = {}
 kvim.lsp.local_include = {}
 kvim.lsp.ghost_text = true
 
-if (vim.fn.isdirectory(vim.fn.stdpath("config") .. "/lua/user")) == 0 then
-    vim.fn.mkdir(vim.fn.stdpath("config") .. "/lua/user")
+
+local user_path = vim.fn.stdpath("config") .. "/lua/user"
+local user_init_path = vim.fn.stdpath("config") .. "/lua/user/init.lua"
+if (vim.fn.isdirectory(user_path)) == 0 then
+    vim.fn.mkdir(user_path)
 end
 
-if (vim.fn.filereadable(vim.fn.stdpath("config") .. "/lua/user/init.lua")) == 0 then
-    local file = io.open(vim.fn.stdpath("config") .. "/lua/user/init.lua", "w")
+if (vim.fn.filereadable(user_init_path)) == 0 then
+    local file = io.open(user_init_path, "w")
     file:write(
         "-- this is where you create your own configuration./n-- This is something that I am not gonna change here/n")
     file:close()
