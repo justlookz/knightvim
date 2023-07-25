6702cacc924bca69d07496b98ac582d02b723d49
justlookz <cspios@gmail.com>
2023-07-25 14:49:50 +0300

    fixed user init file finally

--- lua/knightvim/init.lua
+++ lua/knightvim/init.lua
@@ -10,7 +10,7 @@ if (vim.fn.isdirectory( vim.fn.stdpath("config") .. "/lua/user")) == 0 then
     vim.fn.mkdir( vim.fn.stdpath("config") ..  "/lua/user")
 end
 
-if (vim.fn.exists( vim.fn.stdpath("config") .. "/lua/user/init.lua")) == 0 then
+if (vim.fn.filereadable( vim.fn.stdpath("config") .. "/lua/user/init.lua")) == 0 then
     local file = io.open( vim.fn.stdpath("config") .. "/lua/user/init.lua", "w")
     file:write(
         "-- this is where you create your own configuration./n-- This is something that I am not gonna change here/n")
