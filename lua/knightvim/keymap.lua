ac93905286e43c946e62febad43dcfbd645cedec
justlookz <cspios@gmail.com>
2023-07-28 18:51:31 +0300

    added keymap for window movement

--- lua/knightvim/keymap.lua
+++ lua/knightvim/keymap.lua
@@ -11,13 +11,31 @@ nmap("L", vim.cmd.BufferLineCycleNext, "Next Buffer")
 wk.register({
     ["<leader>s"] = { name = "search" },
     ["<leader>t"] = { name = "Trouble" },
-    ["<leader>sg"] = { name = "Git" }
+    ["<leader>sg"] = { name = "Git" },
+    ["<leader>b"] = { name = "Buffer" }
 })
 
 nmap("<leader>", "<nop>", "Leader Menu")
 nmap("<leader>w", ":write<cr>", "Save")
 nmap("<leader>q", vim.cmd.qall, "Quit")
 nmap("<leader>c", vim.cmd.bdelete, "Quit")
+nmap("<leader>bh", function()
+        vim.cmd.wincmd("h")
+    end,
+    "goto left")
+nmap("<leader>bl", function()
+        vim.cmd.wincmd("l")
+    end,
+    "goto right")
+nmap("<leader>bk", function()
+        vim.cmd.wincmd("k")
+    end,
+    "go above")
+nmap("<leader>bj", function()
+        vim.cmd.wincmd("j")
+    end,
+    "go below")
+
 
 nmap("<leader>sf", require("telescope.builtin").find_files, "Find file")
 nmap("<leader>sh", require("telescope.builtin").help_tags, ":help options")
