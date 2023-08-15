95e9549e9d281f7c93867078b16464878dbdebf7
justlookz <cspios@gmail.com>
2023-08-16 00:40:51 +0300

    cool format on options.lua

--- lua/knightvim/options.lua
+++ lua/knightvim/options.lua
@@ -1,30 +1,30 @@
 vim.g.mapleader = kvim.leader
-local o = vim.o
+local o         = vim.o
 
 -- the list for the options are from
 -- https://github.com/brainfucksec/neovim-lua/blob/main/nvim/lua/core/options.lua
 
 -- General
-o.mouse = 'a'                               -- Enable mouse support
-o.clipboard = 'unnamedplus'                 -- Copy/paste to system clipboard
-o.swapfile = false                          -- Don't use swapfile
-o.completeopt = 'menuone,noinsert,noselect' -- Autocomplete options
+o.mouse         = 'a'                       -- Enable mouse support
+o.clipboard     = 'unnamedplus'             -- Copy/paste to system clipboard
+o.swapfile      = false                     -- Don't use swapfile
+o.completeopt   = 'menuone,noinsert,noselect' -- Autocomplete options
 
 -- Neovim UI
-o.number = true         -- Show line number
-o.showmatch = true      -- Highlight matching parenthesis
-o.foldmethod = 'marker' -- Enable folding (default 'foldmarker')
-o.colorcolumn = '64,78' -- Line lenght marker at 80 columns
-o.splitright = true     -- Vertical split to the right
-o.splitbelow = true     -- Horizontal split to the bottom
-o.ignorecase = true     -- Ignore case letters when search
-o.smartcase = true      -- Ignore lowercase for the whole pattern
-o.linebreak = true      -- Wrap on word boundary
+o.number        = true  -- Show line number
+o.showmatch     = true  -- Highlight matching parenthesis
+o.foldmethod    = 'marker' -- Enable folding (default 'foldmarker')
+o.colorcolumn   = '64,78' -- Line lenght marker at 80 columns
+o.splitright    = true  -- Vertical split to the right
+o.splitbelow    = true  -- Horizontal split to the bottom
+o.ignorecase    = true  -- Ignore case letters when search
+o.smartcase     = true  -- Ignore lowercase for the whole pattern
+o.linebreak     = true  -- Wrap on word boundary
 o.termguicolors = true  -- Enable 24-bit RGB colors
-o.laststatus = 3        -- Set global statusline
-o.encoding = 'utf-8'
-o.confirm = true
-o.shortmess = "IfilnxtToOF"
+o.laststatus    = 3     -- Set global statusline
+o.encoding      = 'utf-8'
+o.confirm       = true
+o.shortmess     = "IfilnxtToOF"
 
 
 -- Tabs, indent
