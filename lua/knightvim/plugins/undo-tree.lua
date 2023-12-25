928e0002e6f4cf6a7a1205de967048d308373ed3
justlookz <cspios@gmail.com>
2023-12-25 16:52:57 +0200

    cleanups

--- lua/knightvim/plugins/undo-tree.lua
+++ lua/knightvim/plugins/undo-tree.lua
@@ -1,8 +1,8 @@
 -- Undo Tree - History like undo
 return {
     'https://github.com/mbbill/undotree',
+    lazy = false,
     keys = {
-
         -- Undo Tree
         { "<leader>uu", vim.cmd.UndotreeToggle, desc = "Undo Tree Structure" },
 
