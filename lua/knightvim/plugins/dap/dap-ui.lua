b5155465567b74c7ae8e168d5871b313f3e00508
justlookz <cspios@gmail.com>
2024-01-26 13:47:30 +0200

    feat: Dap-ui now appear ibto the right side

--- lua/knightvim/plugins/dap/dap-ui.lua
+++ lua/knightvim/plugins/dap/dap-ui.lua
@@ -3,7 +3,36 @@ return {
     dependencies = {
         "https://github.com/mfussenegger/nvim-dap",
     },
-    opts = {},
+    opts = {
+        layouts = { {
+            elements = { {
+                id = "scopes",
+                size = 0.25
+            }, {
+                id = "breakpoints",
+                size = 0.25
+            }, {
+                id = "stacks",
+                size = 0.25
+            }, {
+                id = "watches",
+                size = 0.25
+            } },
+            position = "right",
+            size = 40
+        }, {
+            elements = { {
+                id = "repl",
+                size = 0.5
+            }, {
+                id = "console",
+                size = 0.5
+            } },
+            position = "bottom",
+            size = 10
+        } },
+
+    },
     lazy = true,
     keys = {
         {
