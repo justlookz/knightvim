4fe1366d33bd351446150e45b9cd510bc9ec1faf
justlookz <cspios@gmail.com>
2024-01-27 16:27:41 +0200

    removed trouble. Amazing plugin that never used

--- lua/knightvim/plugins/ui/telescope.lua
+++ lua/knightvim/plugins/ui/telescope.lua
@@ -13,15 +13,8 @@ return {
 
         local telescope = require("telescope")
         --local actions = require("telescope.actions")
-        local trouble = require("trouble.providers.telescope")
 
         telescope.setup {
-            defaults = {
-                mappings = {
-                    i = { ["<c-t>"] = trouble.open_with_trouble },
-                    n = { ["<c-t>"] = trouble.open_with_trouble },
-                },
-            },
             extensions = {
                 fzf = {
                     fuzzy                   = true, -- false will only do exact matching
