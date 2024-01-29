1348dc7c18a48c45928e0afc3bed571713ecea41
justlookz <cspios@gmail.com>
2024-01-29 21:13:34 +0200

    Added Noice with init and learned how it used

---
+++ lua/knightvim/plugins/ui/noice.lua
@@ -0,0 +1,15 @@
+-- lazy.nvim
+return {
+    "https://github.com/folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        messages = { enabled = false },
+        notify = { enabled = false },
+    },
+    dependencies = {
+        "https://github.com/MunifTanjim/nui.nvim",
+    },
+    init = function()
+        vim.o.lazyredraw = false
+    end,
+}
