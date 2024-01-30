a06af8ebe1f5724ecdf73c5d5532c5a0611fa1de
justlookz <cspios@gmail.com>
2024-01-30 02:34:12 +0200

    Fixed signature not working and added some argument for better usage

--- lua/knightvim/plugins/lsp/signature.lua
+++ lua/knightvim/plugins/lsp/signature.lua
@@ -4,8 +4,10 @@ return {
     opts = {
         always_trigger = true,
         auto_close_after = 3,
-        border = "shadow",
+        handler_opts = { border = "shadow" },
         hint_prefix = ";",
+        wrap = true,
+        floating_window_above_cur_line = false,
     },
     config = function(_, opts)
         require 'lsp_signature'.setup(opts)
