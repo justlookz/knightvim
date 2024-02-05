c37df83e2d3aea58e2b037fcdeae168f2ddc2b2c
justlookz <cspios@gmail.com>
2024-02-05 15:39:28 +0200

    Removed height from cmd

--- lua/knightvim/options.lua
+++ lua/knightvim/options.lua
@@ -1,46 +1,47 @@
-vim.g.mapleader      = kvim.leader
+vim.g.mapleader        = kvim.leader
 
 -- the list for the options are from
 -- https://github.com/brainfucksec/neovim-lua/blob/main/nvim/lua/core/options.lua
 
 -- General
-vim.o.mouse          = 'a'                         -- Enable mouse support
-vim.o.clipboard      = 'unnamedplus'               -- Copy/paste to system clipboard
-vim.o.swapfile       = false                       -- Don't use swapfile
-vim.o.completeopt    = 'menuone,noinsert,noselect' -- Autocomplete options
+vim.opt.mouse          = 'a'                         -- Enable mouse support
+vim.opt.clipboard      = 'unnamedplus'               -- Copy/paste to system clipboard
+vim.opt.swapfile       = false                       -- Don't use swapfile
+vim.opt.completeopt    = 'menuone,noinsert,noselect' -- Autocomplete options
 
 -- Neovim UI
-vim.o.number         = true     -- Show line number
-vim.o.showmatch      = true     -- Highlight matching parenthesis
-vim.o.foldmethod     = 'marker' -- Enable folding (default 'foldmarker')
-vim.o.colorcolumn    = '64,78'  -- Line lenght marker at 80 columns
-vim.o.splitright     = true     -- Vertical split to the right
-vim.o.splitbelow     = true     -- Horizontal split to the bottom
-vim.o.ignorecase     = true     -- Ignore case letters when search
-vim.o.smartcase      = true     -- Ignore lowercase for the whole pattern
-vim.o.linebreak      = true     -- Wrap on word boundary
-vim.o.termguicolors  = true     -- Enable 24-bit RGB colors
-vim.o.laststatus     = 3        -- Set global statusline
-vim.o.encoding       = 'utf-8'
-vim.o.confirm        = true
-vim.o.shortmess      = "IfilnxtToOF"
-vim.o.number         = true
-vim.o.relativenumber = true
+vim.opt.number         = true     -- Show line number
+vim.opt.showmatch      = true     -- Highlight matching parenthesis
+vim.opt.foldmethod     = 'marker' -- Enable folding (default 'foldmarker')
+vim.opt.colorcolumn    = '64,78'  -- Line lenght marker at 80 columns
+vim.opt.splitright     = true     -- Vertical split to the right
+vim.opt.splitbelow     = true     -- Horizontal split to the bottom
+vim.opt.ignorecase     = true     -- Ignore case letters when search
+vim.opt.smartcase      = true     -- Ignore lowercase for the whole pattern
+vim.opt.linebreak      = true     -- Wrap on word boundary
+vim.opt.termguicolors  = true     -- Enable 24-bit RGB colors
+vim.opt.laststatus     = 3        -- Set global statusline
+vim.opt.encoding       = 'utf-8'
+vim.opt.confirm        = true
+vim.opt.shortmess      = "IfilnxtToOF"
+vim.opt.number         = true
+vim.opt.relativenumber = true
 
 
 -- Tabs, indent
-vim.o.expandtab = true   -- Use spaces instead of tabs
-vim.o.shiftwidth = 4     -- Shift 4 spaces when tab
-vim.o.tabstop = 4        -- 1 tab == 4 spaces
-vim.o.smartindent = true -- Autoindent new lines
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4     -- Shift 4 spaces when tab
+vim.opt.tabstop = 4        -- 1 tab == 4 spaces
+vim.opt.smartindent = true -- Autoindent new lines
 
--- Memory, CPU{{{
-vim.o.hidden = true     -- Enable background buffers
-vim.o.history = 100     -- Remember N lines in history
-vim.o.lazyredraw = true -- Faster scrolling
-vim.o.synmaxcol = 120   -- Max column for syntax highlight
-vim.o.updatetime = 250  -- ms to wait for trigger an event
-vim.o.undofile = true   -- Persistent Undo History
+-- Memory, CPU
+vim.opt.hidden = true     -- Enable background buffers
+vim.opt.history = 100     -- Remember N lines in history
+vim.opt.lazyredraw = true -- Faster scrolling
+vim.opt.synmaxcol = 120   -- Max column for syntax highlight
+vim.opt.updatetime = 250  -- ms to wait for trigger an event
+vim.opt.undofile = true   -- Persistent Undo History
 
-vim.o.timeout = true
-vim.o.timeoutlen = 300 -- }}}
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
+vim.opt.cmdheight = 0
