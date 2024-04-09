4f3c717b98c45d107b6bea88bd80fac24ef9b09c
justlookz <cspios@gmail.com>
2024-04-09 21:15:11 +0300

    Added monokai pro

--- lua/options.lua
+++ lua/options.lua
@@ -60,3 +60,5 @@ vim.opt.showbreak      = "|"
 vim.opt.foldmethod     = "expr"
 vim.opt.foldexpr       = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevelstart = 99
+vim.opt.spell          = true
+vim.opt.spelllang      = { "el", "en" }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,20 @@
 return {
 
-    "https://github.com/nordtheme/vim", {
-    'https://github.com/embark-theme/vim',
-    name = 'embark'
-},
+    "https://github.com/nordtheme/vim",
+    {
+        'https://github.com/embark-theme/vim',
+        name = 'embark'
+    },
     "https://github.com/EdenEast/nightfox.nvim", {
     'https://github.com/rose-pine/neovim',
     name = 'rose-pine',
 },
     "https://github.com/folke/tokyonight.nvim",
     "https://github.com/LunarVim/lunar.nvim",
+    {
+        "loctvl842/monokai-pro.nvim",
+        config = function()
+            require("monokai-pro").setup()
+        end
+    }
 }
