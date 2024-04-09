692bb188b62fea55c00b29daea2d2a7bddddaf73
justlookz <cspios@gmail.com>
2024-04-09 23:38:37 +0300

    better keymap

--- lua/plugins/ui/oil.lua
+++ lua/plugins/ui/oil.lua
@@ -5,8 +5,22 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
         {
-            "<leader>o",
-            "<cmd>Oil<cr>",
+            "<leader>oo",
+            function()
+                if vim.opt.filetype:get() ~= "oil" then
+                    require("oil").open()
+                else
+                    require("oil").close()
+                end
+            end,
+            desc = "Oil File manager",
+        },
+
+        {
+            "<leader>of",
+            function()
+                require("oil").toggle_float()
+            end,
             desc = "Oil File manager",
         }
     }
